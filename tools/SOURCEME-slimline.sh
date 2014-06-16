7bf864567e1001b4d9c9ab948277b779f171e08d
John Rigby <john.rigby@vivint.com>
2014-06-16 16:02:20 -0600

    Updates
    Use make ext4.gz.u-boot the default rootfs type for
    using as an initrd image.
    
    Update SOURCEME-slimline

--- tools/SOURCEME-slimline.sh
+++ tools/SOURCEME-slimline.sh
@@ -3,4 +3,4 @@ export EULA=1
 source fsl-setup-release.sh -e fb -b build
 grep -s meta-vivint $BUILD_DIR/conf/bblayers.conf ||
 	echo "BBLAYERS += \" \${BSPDIR}/sources/meta-vivint \"" >> $BUILD_DIR/conf/bblayers.conf
-ln -s ../sstat* .
+test ../sstate-cache && ln -s ../sstate-cache .
