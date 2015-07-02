27cf72918886aac1fb890e850964c14413ff43a4
John Rigby <john.rigby@vivint.com>
2015-07-02 14:16:29 -0600

    SLM-98: new SOURCEME-slimline.sh
    This one works without the fsl-setup-release since that is not
    part of upstream community bsp.
    
    and remove other unused SOURCEME* files

--- tools/SOURCEME-slimline.sh
+++ tools/SOURCEME-slimline.sh
@@ -1,12 +1,17 @@
 export MACHINE=imx6dl-slimline
 export EULA=1
 export DISTRO=slimline
-source fsl-setup-release.sh -e fb -b build
+export BUILD_DIR=build
+source ./setup-environment $BUILD_DIR
+# reset BUILD_DIR to . because above cd'd to BUILD_DIR
+export BUILD_DIR=.
 grep -q meta-vivint $BUILD_DIR/conf/bblayers.conf ||
 	echo "BBLAYERS += \" \${BSPDIR}/sources/meta-vivint \"" >> $BUILD_DIR/conf/bblayers.conf
 ln -s ../sstate-cache . || true
 grep -q package_rpm $BUILD_DIR/conf/local.conf &&
 	sed -i -e s/package_rpm/package_ipk/ $BUILD_DIR/conf/local.conf
+grep -q DIST_FEATURES_remove $BUILD_DIR/conf/local.conf ||
+	echo "DISTRO_FEATURES_remove=\"x11 wayland directfb \"" >> $BUILD_DIR/conf/local.conf
 grep -q HG_SERVER $BUILD_DIR/conf/local.conf ||
 cat << '_END_' >> $BUILD_DIR/conf/local.conf
 HG_SERVER = "scm.vivint.com/hg"
