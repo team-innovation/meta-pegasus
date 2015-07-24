f3d858c92e91ffdffad8f67c51f2577feff1fc15
John Rigby <john.rigby@vivint.com>
2015-07-23 18:08:59 -0600

    SOURCEME-slimline.sh: do the right thing
    finally got it right:
    
    if passed an argument
    	use it as build dir name
    otherwise
    	use "build"
    
    if builddir exists
    	just run setup-environment
    otherwise
    	do the initial setup
    	which includes running the fsl setup
    	followed by our modifications

--- tools/SOURCEME-slimline.sh
+++ tools/SOURCEME-slimline.sh
@@ -1,28 +1,37 @@
+export BUILD_DIR=$1
+# use "build" as the default build dir
+: ${BUILD_DIR:=build}
+
+# if already set up then we just run the setup-environment script
+test -d ${BUILD_DIR} &&
+	grep -q meta-vivint ${BUILD_DIR}/conf/bblayers.conf &&
+	source setup-environment ${BUILD_DIR} &&
+	return
+
+# it is a new dir
+echo Setting up new dir \"${BUILD_DIR}\"
 export MACHINE=imx6dl-slimline
 export EULA=1
 export DISTRO=slimline
-export BUILD_DIR=build
-source ./fsl-setup-release.sh $BUILD_DIR
-# reset BUILD_DIR to . because above cd'd to BUILD_DIR
-export BUILD_DIR=.
 
-grep -q meta-ruby $BUILD_DIR/conf/bblayers.conf ||
-        echo "BBLAYERS += \" \${BSPDIR}/sources/meta-openembedded/meta-ruby \"" >> $BUILD_DIR/conf/bblayers.conf
+source ./fsl-setup-release.sh -b ${BUILD_DIR}
 
-grep -q meta-vivint $BUILD_DIR/conf/bblayers.conf ||
-        echo "BBLAYERS += \" \${BSPDIR}/sources/meta-vivint \"" >> $BUILD_DIR/conf/bblayers.conf
-
-grep -q meta-qt5 $BUILD_DIR/conf/bblayers.conf ||
-        echo "BBLAYERS += \" \${BSPDIR}/sources/meta-qt5 \"" >> $BUILD_DIR/conf/bblayers.conf
+grep -q meta-vivint ./conf/bblayers.conf ||
+	echo "BBLAYERS += \" \${BSPDIR}/sources/meta-vivint \"" \
+		>> ./conf/bblayers.conf
 
 ln -s ../sstate-cache . || true
-grep -q package_rpm $BUILD_DIR/conf/local.conf &&
-	sed -i -e s/package_rpm/package_ipk/ $BUILD_DIR/conf/local.conf
-grep -q DIST_FEATURES_remove $BUILD_DIR/conf/local.conf ||
-	echo "DISTRO_FEATURES_remove=\"x11 wayland directfb \"" >> $BUILD_DIR/conf/local.conf
-grep -q HG_SERVER $BUILD_DIR/conf/local.conf ||
-cat << '_END_' >> $BUILD_DIR/conf/local.conf
-HG_SERVER = "scm.vivint.com/hg"
-HG_APPS_TAG ?= "apps-hg"
-HG_APPS_ID ?= "slimline"
-_END_
+
+grep -q package_rpm ./conf/local.conf &&
+	sed -i -e s/package_rpm/package_ipk/ ./conf/local.conf
+
+grep -q DIST_FEATURES_remove ./conf/local.conf ||
+	echo "DISTRO_FEATURES_remove=\"x11 wayland directfb \"" \
+		>> ./conf/local.conf
+
+grep -q HG_SERVER ./conf/local.conf ||
+	cat <<-'_END_' >> ./conf/local.conf
+		HG_SERVER = "scm.vivint.com/hg"
+		HG_APPS_TAG ?= "apps-hg"
+		HG_APPS_ID ?= "slimline"
+	_END_
