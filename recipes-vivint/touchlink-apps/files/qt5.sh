2946097f4601a9d4d91631f7db271d543253d8b3
Johny Bui <jbui@vivint.com>
2015-08-04 14:07:19 -0600

    Disable console cursor and framebuffer timeout

--- recipes-vivint/touchlink-apps/files/qt5.sh
+++ recipes-vivint/touchlink-apps/files/qt5.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Disable framebuffer console cursor
+echo -e '\033[?17;0;0c' > /dev/tty0
+
+# Disable framebuffer timeout
+echo -e '\033[9;0]' > /dev/tty1
+
 export QT_QPA_EGLFS_PHYSICAL_HEIGHT=90
 export QT_QPA_EGLFS_PHYSICAL_WIDTH=153
 export QT_QPA_EGLFS_WIDTH=1024
