247928c8f23a303cc2bfef0c3bc0a35909e08a2a
John Rigby <john.rigby@vivint.com>
2016-04-18 10:02:08 -0600

    audio_sh: use sysclass driver instead of raw gpios

--- recipes-vivint/audio-sh/files/audio.sh
+++ recipes-vivint/audio-sh/files/audio.sh
@@ -1,38 +1,25 @@
 #!/bin/bash
-export SD_BOOST=136
-export FB_SEL=140
-export SD_AMP=144
-export SS_FF=145
-export AUD_RESET=107
-
-gpiodiroutset() {
-	local gpio=$1
-	local val=$2
-	test -d /sys/class/gpio/gpio${gpio} || {
-		echo "$1" > /sys/class/gpio/export
-		echo "out" > /sys/class/gpio/gpio${gpio}/direction
-	}
-	echo "$val" > /sys/class/gpio/gpio${gpio}/value
-}
-
 rmmods() {
-	rmmod snd_soc_imx_cx20704 || true
-	rmmod snd_soc_cx20704 || true
+	lsmod | grep -q snd_soc_imx_cx20704 &&
+		rmmod snd_soc_imx_cx20704
+	lsmod | grep -q snd_soc_cx20704 &&
+		rmmod snd_soc_cx20704
 }
 
 initall() {
-	gpiodiroutset ${SD_BOOST} 1
-	gpiodiroutset ${FB_SEL} 1
-	gpiodiroutset ${SD_AMP} 1
-	gpiodiroutset ${SS_FF} 1
-	gpiodiroutset ${AUD_RESET} 1
+	echo 1 > /sys/class/lm48511/lm485110/access
+	echo 0 > /sys/class/lm48511/lm485110/aud_reset
+	echo 1 > /sys/class/lm48511/lm485110/sd_amp
+	echo 1 > /sys/class/lm48511/lm485110/sd_boost
+	echo 1 > /sys/class/lm48511/lm485110/fb_sel
+	echo 1 > /sys/class/lm48511/lm485110/ss_ff
+	echo 1 > /sys/class/lm48511/lm485110/aud_reset
 }
 
-#rmmods
+rmmods
 initall
 sleep 1
 modprobe snd_soc_cx20704
 modprobe snd_soc_imx_cx20704
 amixer sset 'I2S TX Source' 'Stream2: mic ADC'
 amixer sset 'DAC Source' 'Stream3: I2S in'
-
