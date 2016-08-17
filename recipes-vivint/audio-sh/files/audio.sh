9b6844c65eeb08c8593947749dc92daca366a543
John Rigby <john.rigby@vivint.com>
2016-08-17 13:47:15 -0600

    SC-4331: only reset codec if driver module is not loaded
    Resetting the codec when the driver is loaded results in the registers
    being reset back to defaults but the driver view is still of the
    patched versions of the registers.
    
    Rerunning audio.sh is only done when debugging so this will be less
    confusing for the debug case and never an issue for the normal case.
    
    Also added some debug code to dump regs before and after the fixup.
    Registers land in /tmp/cxregs.  We can remove this later after we are
    certain all is working fine.

--- recipes-vivint/audio-sh/files/audio.sh
+++ recipes-vivint/audio-sh/files/audio.sh
@@ -8,12 +8,67 @@ rmmods() {
 
 initall() {
 	echo 1 > /sys/class/lm48511/lm485110/access
-	echo 0 > /sys/class/lm48511/lm485110/aud_reset
+	lsmod | grep -q snd_soc_cx20704 ||
+		echo 0 > /sys/class/lm48511/lm485110/aud_reset
 	echo 1 > /sys/class/lm48511/lm485110/sd_amp
 	echo 1 > /sys/class/lm48511/lm485110/sd_boost
 	echo 1 > /sys/class/lm48511/lm485110/fb_sel
 	echo 1 > /sys/class/lm48511/lm485110/ss_ff
-	echo 1 > /sys/class/lm48511/lm485110/aud_reset
+	lsmod | grep -q snd_soc_cx20704 ||
+		echo 1 > /sys/class/lm48511/lm485110/aud_reset
+}
+
+dumpreg() {
+	echo $1 > regread
+	echo -n "$1: " >> /tmp/cxregs
+	cat regread >> /tmp/cxregs
+}
+
+dumpregs() {
+	echo '===================' >> /tmp/cxregs
+	echo regs $1 >> /tmp/cxregs
+	echo '===================' >> /tmp/cxregs
+	cd /sys/class/cx20704/cx20704_controls
+	echo '===================' >> /tmp/cxregs
+	dumpreg 0x0f51
+	dumpreg 0x101e
+	dumpreg 0x116b
+	dumpreg 0x116d
+	dumpreg 0x116e
+	dumpreg 0x1171
+	dumpreg 0x1173
+	dumpreg 0x1176
+	dumpreg 0x117a
+	dumpreg 0x117f
+	dumpreg 0x1181
+	dumpreg 0x1184
+	dumpreg 0x1186
+	dumpreg 0x15c9
+	dumpreg 0x15ca
+	dumpreg 0x15cb
+	dumpreg 0x15cc
+	dumpreg 0x15cd
+	dumpreg 0x15ce
+	dumpreg 0x15cf
+	dumpreg 0x15d0
+	dumpreg 0x15d1
+	dumpreg 0x15d2
+	dumpreg 0x15d3
+	dumpreg 0x15d4
+	dumpreg 0x15d5
+	dumpreg 0x15d6
+	dumpreg 0x15d7
+	dumpreg 0x15d8
+	dumpreg 0x15d9
+	dumpreg 0x15da
+	dumpreg 0x15db
+	dumpreg 0x15dc
+	dumpreg 0x15e3
+	dumpreg 0x15e4
+	dumpreg 0x117b
+	dumpreg 0x117c
+	dumpreg 0x117d
+	echo '===================' >> /tmp/cxregs
 }
 
 patch_aec() {
@@ -98,7 +153,9 @@ modprobe snd_soc_imx_cx20704
 amixer sset 'I2S TX Source' 'Stream2: mic ADC'
 amixer sset 'DAC Source' 'Stream3: I2S in'
 sleep 1
+dumpregs before
 patch_aec
+dumpregs after
 
 grep -q sly /proc/device-tree/compatible && {
 	modprobe snd_soc_gsm030x
