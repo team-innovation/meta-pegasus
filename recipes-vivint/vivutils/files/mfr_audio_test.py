69598f8864efd07e07cac3aed06b231cbff8ccf7
Tanner Oakes <tanner.oakes@vivint.com>
2016-12-21 16:09:44 -0700

    Restart pulseaudio before running audio test

--- recipes-vivint/vivutils/files/mfr_audio_test.py
+++ recipes-vivint/vivutils/files/mfr_audio_test.py
@@ -69,6 +69,9 @@ class AudioTest:
         # force off noise reduction and echo cancellation
         os.system("echo 0x117a 0x8 > /sys/class/cx20704/cx20704_controls/regwrite")
         os.system("echo 1 > /sys/class/cx20704/cx20704_controls/newc")
+
+        # restart pulseaudio to get a good connection
+        os.system("/etc/init.d/pulseaudio restart  > /dev/null")
         
         self._enable_sly_audio_amp()
 
