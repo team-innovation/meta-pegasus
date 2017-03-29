0fac636463655d44a48ebad55520adeb8e358747
bvernon <bruce.vernon@vivint.com>
2017-03-29 15:35:23 -0600

    Add OPENWRT_BRANCH variable to the environment

--- tools/SOURCEME-slimline.sh
+++ tools/SOURCEME-slimline.sh
@@ -4,14 +4,17 @@ export BUILD_DIR=$1
 
 _APPS_TAG="apps-hg"
 _APPS_ID="default"
+_OPENWRT_BRANCH="check"
 
 # if already set up then we just run the setup-environment script
 test -d ${BUILD_DIR} &&
 	sed -i '/HG_APPS_TAG/d' ${BUILD_DIR}/conf/local.conf &&
 	sed -i '/HG_APPS_ID/d' ${BUILD_DIR}/conf/local.conf && 
+	sed -i '/OPENWRT_BRANCH/d' ${BUILD_DIR}/conf/local.conf && 
 	sed -i '/UPDATESENG/d' ${BUILD_DIR}/conf/local.conf && 
 	echo "HG_APPS_TAG ?= \"${_APPS_TAG}\"" >> ${BUILD_DIR}/conf/local.conf &&
 	echo "HG_APPS_ID ?= \"${_APPS_ID}\"" >> ${BUILD_DIR}/conf/local.conf &&
+	echo "OPENWRT_BRANCH ?= \"${_OPENWRT_BRANCH}\"" >> ${BUILD_DIR}/conf/local.conf &&
 	echo "UPDATESENG ?= \"updateseng.vivint.com/innovation\"" >> ${BUILD_DIR}/conf/local.conf &&
 	grep -q meta-vivint ${BUILD_DIR}/conf/bblayers.conf &&
 	grep -q oe-meta-go ${BUILD_DIR}/conf/bblayers.conf &&
@@ -44,4 +47,4 @@ grep -q DIST_FEATURES_remove ./conf/local.conf ||
 		>> ./conf/local.conf
 
 grep -q HG_SERVER ./conf/local.conf || 
-	echo -e "HG_SERVER = \"scm.vivint.com/hg\"\nHG_APPS_TAG ?= \"${_APPS_TAG}\"\nHG_APPS_ID ?= \"${_APPS_ID}\"\nUPDATESENG = \"updateseng.vivint.com/innovation\"" >> ./conf/local.conf
+	echo -e "HG_SERVER = \"scm.vivint.com/hg\"\nHG_APPS_TAG ?= \"${_APPS_TAG}\"\nHG_APPS_ID ?= \"${_APPS_ID}\"\nOPENWRT_BRANCH ?= \"${_OPENWRT_BRANCH}\"\nUPDATESENG = \"updateseng.vivint.com/innovation\"" >> ./conf/local.conf
