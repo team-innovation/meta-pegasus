b1e7ed39f8efbd02f1478fe159f86a8cfb0515e0
Craig <cmatsuura@vivint.com>
2018-11-26 17:58:46 -0700

    Auto refresh page after submit

--- recipes-vivint/vis/files/graph_me.sh
+++ recipes-vivint/vis/files/graph_me.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
-python3 /usr/bin/get_mesh_info.py
-python3 /usr/bin/build_dot_graph.py
+export PYTHONUNBUFFERED=1
+/usr/bin/python3 /usr/bin/get_mesh_info.py &> /dev/null
+/usr/bin/python3 /usr/bin/build_dot_graph.py &> /dev/null
+
+echo "Content-type: text/html"
+echo ""
+
+echo '<html>'
+echo '<head>'
+echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'
+echo '<title>Hello World</title>'
+echo '</head>'
+echo '<body>'
+if [ "$REQUEST_METHOD" = "GET" ]
+then
+                echo "<meta http-equiv=\"refresh\" content=\"0; URL=$HTTP_REFERER\" />"
+        fi
+        echo '</body>'
+        echo '</html>'
+
+        exit 0
+
