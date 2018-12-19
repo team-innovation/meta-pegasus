e76183bfc81017c9c1a5444625df21ea1c081229
Tanner Oakes <tanner.oakes@vivint.com>
2018-12-19 11:27:33 -0700

    Add linux mfgtool to mfgkit

---
+++ recipes-vivint/mfgkit/files/mfgtool/wallsly-v6-reflash-plus-serial.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+if [ ! -f /etc/udev/rules.d/51-imx-permissions.rules ]; then
+	echo "******************************************"
+	echo "Copy 51-imx-permissions.rules to /etc/udev/rules.d or run as root to have libusb permission"
+	echo "******************************************"
+	echo
+fi
+
+./mfgtoolcli -c "Linux" -l "Wallsly-v6-Reflash-Plus-Serial"
