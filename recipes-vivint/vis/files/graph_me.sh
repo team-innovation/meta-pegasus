cc4ba0bd9596bf7d1fc5a85f43167cd0839a0ab1
Craig <cmatsuura@vivint.com>
2019-03-22 13:01:28 -0600

    Trying to get the page to not cache on the browser.
    Also prevent graph_me.sh from running hte python scripts more than once

--- recipes-vivint/vis/files/graph_me.sh
+++ recipes-vivint/vis/files/graph_me.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 export PYTHONUNBUFFERED=1
-/usr/bin/python3 /usr/bin/get_mesh_info.py &> /var/log/get_mesh_info.log
-/usr/bin/python3 /usr/bin/build_dot_graph.py &> /var/log/build_dot_graph.log
+RUNNING=/tmp/graph_me.run
+if [ ! -e $RUNNING ] ; then
+    logger -t DEBUG "Run graph_me!"
+    date > $RUNNING
+    /usr/bin/python3 /usr/bin/get_mesh_info.py &> /var/log/get_mesh_info.log
+    /usr/bin/python3 /usr/bin/build_dot_graph.py &> /var/log/build_dot_graph.log
+    rm $RUNNING
+    logger -t DEBUG "Done graph_me!"
+else
+    logger -t DEBUG "graph_me is already running!"
+    while [ -e $RUNNING ] ; do
+	sleep 1
+    done
+fi
 
 echo "Content-type: text/html"
 echo ""
