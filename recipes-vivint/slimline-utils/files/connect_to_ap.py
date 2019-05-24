a4e624976f413aaf0f4fec4fc3caaff1b5a426dd
Tanner Oakes <tanner.oakes@vivint.com>
2019-05-24 14:06:26 -0600

    wifi connect glycerine glance scripts

--- recipes-vivint/slimline-utils/files/connect_to_ap.py
+++ recipes-vivint/slimline-utils/files/connect_to_ap.py
@@ -7,27 +7,25 @@ import os
 import time
 from taurine.rpc.rpc_client import RpcClient
 from taurine.async_io.event_loop import EventLoop
-from taurine.async_io.delayed_call import DelayedCall
 
 cgitb.enable()
 
 sys.path.insert(0, "/opt/2gig/netd/proxies/python")
 sys.path.insert(0, "/opt/2gig/netd")
-
 class NetworkHelper(EventLoop):
     def __init__(self):
-        super().__init__(self.on_exit)
+        super().__init__()
         self.netd = None
         self.form = cgi.FieldStorage()
-        self.ssid = self.form.getvalue("ssid", "")
+        self.ssid =  self.form.getvalue("ssid", "")
         self.password = self.form.getvalue("password","")
 
-    def setup(self):
+    async def setup(self):
         self.__client = RpcClient("netd")
         self.__client.connect('localhost', 7170, self.on_connected, auto_reconnect=False)
 
     def run(self):
-        DelayedCall(0, self.setup)
+        self.create_task(self.setup())
         super().run()
 
     def on_connected(self, error):
@@ -36,26 +34,22 @@ class NetworkHelper(EventLoop):
             self.connect_to_panel()
 
     def connect_to_panel(self):
+        self.netd.start_wifi_connect(self.ssid, self.password)
+        self.shutdown()
+
+try:
+    pid = os.fork()
+    if pid:
         print("Content-type: text/html")
-        print("<html><head><title>Connecting</title></head><body><Connecting></body></html>")
-        sys.stdout.flush()
-        os.close(sys.stdout.fileno())
+        print("")
+        print("<html><head><title>Connecting</title></head><body>Connecting</body></html>")
+        sys.exit(0)
+except OSError:
+    sys.exit(1)
 
-        try:
-            pid = os.fork()
-            if pid:
-                sys.exit(0)
-            
-            time.sleep(1)
-            os.setsid()
-
-            self.netd.start_wifi_connect(self.ssid, self.password)
-            os._exit(0)
-        except OSError as e:
-            sys.exit(1)
-
-    def on_exit(self):
-        pass
+os.setsid()
+sys.stdout.flush()
+os.close(sys.stdout.fileno())
 
 nh = NetworkHelper()
 nh.run()
