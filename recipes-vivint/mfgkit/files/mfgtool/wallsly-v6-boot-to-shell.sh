dc8db54de89c22a7f8c2b177e67141a793926fdf
Rakesh R <reddyrakesh926@gmail.com>
2019-08-05 11:13:20 -0600

    Rebase on staging Aug-05.

--- recipes-vivint/mfgkit/files/mfgtool/wallsly-v6-boot-to-shell.sh
+++ recipes-vivint/mfgkit/files/mfgtool/wallsly-v6-boot-to-shell.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 if [ ! -f /etc/udev/rules.d/51-imx-permissions.rules ]; then
 	echo "******************************************"
 	echo "Copy 51-imx-permissions.rules to /etc/udev/rules.d or run as root to have libusb permission"
