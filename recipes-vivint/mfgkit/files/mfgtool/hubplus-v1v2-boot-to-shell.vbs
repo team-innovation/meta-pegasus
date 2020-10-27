ed5fbc3f7f10ab4179445b613729b3500f689d13
John Rigby <john.rigby@vivint.com>
2020-10-26 20:17:56 -0600

    Add Hubplus-v2 mfgtool support
    Since it is compatible with v1, just reuse renamed v1 scripts.

---
+++ recipes-vivint/mfgkit/files/mfgtool/hubplus-v1v2-boot-to-shell.vbs
@@ -0,0 +1,3 @@
+Set wshShell = CreateObject("WScript.shell")
+wshShell.run "mfgtool2.exe -c ""linux"" -l ""Hubplus-v1-Boot-to-shell"" "
+Set wshShell = Nothing
---
+++ recipes-vivint/mfgkit/files/mfgtool/hubplus-v1v2-reflash-plus-serial.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ ! -f /etc/udev/rules.d/99-imx-permissions.rules ]; then
+	echo "******************************************"
+	echo "Copy 99-imx-permissions.rules to /etc/udev/rules.d or run as root to have libusb permission"
+	echo "******************************************"
+	echo
+fi
+
+./mfgtoolcli -c "Linux" -l "Hubplus-v1-Reflash-Plus-Serial"
