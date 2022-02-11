040e666595c48b5e36c92a7c01cff976eeed0486
johny <jbui@vivint.com>
2022-02-11 11:03:03 -0700

    EMB-11749: add p2p0 conf, remove default OPEN network profile.

--- recipes-connectivity/wpa-supplicant/wpa-supplicant/wpa-supplicant.sh
+++ recipes-connectivity/wpa-supplicant/wpa-supplicant/wpa-supplicant.sh
@@ -6,7 +6,6 @@ WPA_SUP_PNAME="wpa_supplicant"
 WPA_SUP_PIDFILE="/var/run/wpa_supplicant.$IFACE.pid"
 WPA_COMMON_CTRL_IFACE="/var/run/wpa_supplicant"
 WPA_SUP_OPTIONS="-B -P $WPA_SUP_PIDFILE -i $IFACE"
-
 VERBOSITY=0
 
 
@@ -47,9 +46,11 @@ if [ "$MODE" = "start" ] ; then
         # if we have passed the criteria, start wpa_supplicant
         if [ -n "$WPA_SUP_CONF" ]; then
 		if [ $platform'x' == 'nenex' ] || [ $platform'x' == 'gallusx' ]; then
-			IF_WPA_DRIVER_OPTION=""
+			IF_WPA_DRIVER_OPTION="-Dnl80211"
+			WPA_P2P0_OPTIONS="-N -i p2p0 $IF_WPA_DRIVER_OPTION -c /etc/wpa_supplicant_p2p0.conf -puse_p2p_group_interface=1"
 		else
-			IF_WPA_DRIVER_OPTION="-D $IF_WPA_DRIVER"
+			IF_WPA_DRIVER_OPTION="-D$IF_WPA_DRIVER"
+			WPA_P2P0_OPTIONS=""
 		fi
 
 
@@ -59,7 +60,7 @@ if [ "$MODE" = "start" ] ; then
 
                 start-stop-daemon --start --quiet \
                         --name $WPA_SUP_PNAME --startas $WPA_SUP_BIN --pidfile $WPA_SUP_PIDFILE \
-                        --  $WPA_SUP_OPTIONS $WPA_SUP_CONF $IF_WPA_DRIVER_OPTION
+                        --  $WPA_SUP_OPTIONS $IF_WPA_DRIVER_OPTION $WPA_SUP_CONF $WPA_P2P0_OPTIONS
         fi
 
         # if the interface socket exists, then wpa_supplicant was invoked successfully
--- recipes-connectivity/wpa-supplicant/wpa-supplicant_%.bbappend
+++ recipes-connectivity/wpa-supplicant/wpa-supplicant_%.bbappend
@@ -1,2 +1,12 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 
+PR = "r2"
+
+SRC_URI += "file://wpa_supplicant_p2p0.conf"
+
+do_install_append() {
+	install -d ${D}${sysconfdir}
+	install -m 0644 ${WORKDIR}/wpa_supplicant_p2p0.conf ${D}/${sysconfdir}
+}
+
+FILES_${PN} += "${sysconfdir}/wpa_supplicant_p2p0.conf"
